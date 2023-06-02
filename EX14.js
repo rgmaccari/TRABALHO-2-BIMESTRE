82825809b1b2498a8b784cda92ee473eb9ed11cd
Rafael Gustavo Maccari <r.maccari@edu.unipar.br>
2023-06-01 22:51:07 -0300

    Comentário sobre o item 14

--- EX14.js
+++ EX14.js
@@ -9,6 +9,7 @@ function racharaconta(){
     let valoresfechados = conta-(rachaoemtres*2);
 /* Multiplica o rachao ja arredondado por 2 = Andre e Carlos */
 /* Subtrai o valor total da conta, o que sobrar é do Felipe */
+/* Números inteiros */
     resultados.innerHTML = 'Felipe pagará R$'+valoresfechados+
     '. Enquanto André e Carlos pagarão R$'+rachaoemtres+
     ' cada.';
